28b84436e8e1e31bb650838ff151ed69dda6ff49
tsaie79 <tsaie79@gmail.com>
2023-12-04 16:59:10 -0500

    Add function to write command to FIFO

---
+++ fifo/write_to_fifo.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    // "io"
+    "os"
+    "syscall"
+	"github.com/containerd/fifo"
+	// "os/exec"
+    "strings"
+)
+
+
+func writeCmdToFifo(command []string, args []string, env map[string]interface{}) error {
+    fifoPath := "/home/jeng-yuantsai/Documents/JIRIAF/virtual-kubelet-cmd/fifo/hostpipe"
+    ctx := context.Background()
+    fn := fifoPath + "/myFifo"
+    flag := syscall.O_WRONLY
+    perm := os.FileMode(0666)
+
+    fifo, err := fifo.OpenFifo(ctx, fn, flag, perm)
+    if err != nil {
+        fmt.Printf("Error opening FIFO: %v\n", err)
+        return err
+    }
+
+    // write env to a single string like "export key1='value1'&& export key2='value2'..."
+    var envString string
+    for key, value := range env {
+        // if type of value is string, use single quotes to prevent shell from interpreting the value
+        switch value.(type) {
+        case string:
+            envString += "export " + key + "=\"" + value.(string) + "\" && "
+        //if type of value is int or float, no quotes are needed
+        case int:
+            envString += "export " + key + "=" + fmt.Sprintf("%d", value.(int)) + " && "
+        case float64:
+            envString += "export " + key + "=" + fmt.Sprintf("%f", value.(float64)) + " && "
+        }   
+    }
+
+
+    cmdString := strings.Join(command, " ")
+    argsString := strings.Join(args, " ")
+    //use single quotes to around the argsString to prevent shell from interpreting the args
+    cmd := cmdString + " '" + envString + argsString + "'"
+
+    fmt.Printf("cmd: %s\n", cmd)
+
+    _, err = fifo.Write([]byte(cmd))
+    if err != nil {
+        fmt.Printf("Error writing to FIFO: %v\n", err)
+        return err
+    }
+
+    return nil
+}
+
+
+func main() {
+    cmds := []string{"/bin/bash", "-c"}
+    args := []string{"bash /home/jeng-yuantsai/Documents/JIRIAF/virtual-kubelet-cmd/fifo/script.sh"}
+    env := map[string]interface{}{"message": "hello world"}
+    err := writeCmdToFifo(cmds, args, env)
+    if err != nil {
+        fmt.Printf("Error writing to FIFO: %v\n", err)
+    }
+}
