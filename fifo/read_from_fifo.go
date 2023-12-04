63092084c8283d53b31655ca8de8f1547d814e50
tsaie79 <tsaie79@gmail.com>
2023-12-04 16:59:01 -0500

    Add readCmdFromFifo function to read and execute commands from a FIFO pipe

---
+++ fifo/read_from_fifo.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    // "io"
+    "os"
+    "syscall"
+	"github.com/containerd/fifo"
+	"os/exec"
+)
+
+func readCmdFromFifo() {
+    fifoPath := "/home/jeng-yuantsai/Documents/JIRIAF/virtual-kubelet-cmd/fifo/hostpipe"
+    ctx := context.Background()
+    fn := fifoPath + "/myFifo"
+    flag := syscall.O_RDONLY | syscall.O_CREAT 
+    perm := os.FileMode(0666)
+
+
+	for {
+		fifo, err := fifo.OpenFifo(ctx, fn, flag, perm)
+		if err != nil {
+			fmt.Printf("Error opening FIFO: %v\n", err)
+			return
+		}
+		// defer fifo.Close()
+
+		buf := make([]byte, 1024)
+		n, err := fifo.Read(buf)
+		if err != nil {
+			fmt.Printf("Error reading from FIFO: %v\n", err)
+			return
+		}
+		fmt.Printf("Read %d bytes from FIFO: %s\n", n, buf[:n])
+
+		// run cmd from fifo output
+		read_cmd := string(buf[:n])
+		env := os.Environ()
+		cmd := exec.Command(read_cmd)
+		cmd.Env = env
+
+		// cmd.Env = append(cmd.Env, "message=from fifo")
+		// cmd.Env = append(cmd.Env, "message2=from fifo2")
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		
+		if err != nil {
+			fmt.Printf("Error running command: %v\n", err)
+			return
+		}
+
+		//write stdout and stderr to files via 
+
+
+		// get pid of cmd
+		fmt.Printf("PID: %d\n", cmd.Process.Pid)
+		fifo.Close()
+	}
+}
+
+func main() {
+	readCmdFromFifo()
+}
