6f6b6b8eda2a5ef1a012d67970efa86d28b89fae
tsaie79 <tsaie79@gmail.com>
2023-12-04 04:56:47 +0000

    Remove debug log level from virtual-kubelet command

--- test-run/start.sh
+++ test-run/start.sh
@@ -13,4 +13,6 @@ export NODENAME="vk-config-test"
 
 echo "{\"$NODENAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > $HOME/.host-cfg.json
 
-"$VK_BIN/virtual-kubelet" --nodename $NODENAME --provider mock --provider-config $HOME/.host-cfg.json --log-level debug --klog.v 3 > ./vk.log 2>&1 
+"$VK_BIN/virtual-kubelet" --nodename $NODENAME --provider mock --provider-config $HOME/.host-cfg.json --klog.v 3 > ./vk.log 2>&1 
+# "$VK_BIN/virtual-kubelet" --nodename $NODENAME --provider mock --provider-config $HOME/.host-cfg.json --log-level debug --klog.v 3 > ./vk.log 2>&1 
+# 
