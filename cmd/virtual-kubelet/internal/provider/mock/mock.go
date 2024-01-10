a8093993290332603c3e71d7f75549286f0faf18
tsaie79 <tsaie79@gmail.com>
2024-01-10 06:12:45 +0000

    Update mock provider to include CPU and memory metrics

--- cmd/virtual-kubelet/internal/provider/mock/mock.go
+++ cmd/virtual-kubelet/internal/provider/mock/mock.go
@@ -27,6 +27,9 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	// "github.com/pkg/errors"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+
 )
 
 const (
@@ -207,9 +210,15 @@ func (p *MockProvider) CreatePod(ctx context.Context, pod *v1.Pod) error {
 		}
 	}
 
-	pod.Status.Phase = v1.PodSucceeded
-	pod.Status.Reason = "PodSucceeded"
-	pod.Status.Message = "Commands succeeded to execute"
+	// pod.Status.Phase = v1.PodSucceeded
+	// pod.Status.Reason = "PodSucceeded"
+	// pod.Status.Message = "Commands succeeded to execute"
+	// pod.Status.StartTime = &start_time
+    
+
+	pod.Status.Phase = v1.PodRunning
+	pod.Status.Reason = "PodRunning"
+	pod.Status.Message = "Commands are executing"
 	pod.Status.StartTime = &start_time
 
 	p.notifier(pod)
@@ -574,18 +583,31 @@ func (p *MockProvider) GetStatsSummary(ctx context.Context) (*stats.Summary, err
 	return res, nil
 }
 
+
 func (p *MockProvider) generateMockMetrics(metricsMap map[string][]*dto.Metric, resourceType string, label []*dto.LabelPair) map[string][]*dto.Metric {
 	var (
 		cpuMetricSuffix    = "_cpu_usage_seconds_total" // the rate of change of this metric is the cpu usage
 		memoryMetricSuffix = "_memory_working_set_bytes"
-		cpuDummyValue      = time.Since(p.startTime).Seconds() // use the time since it's a monotonically increasing value
-		memoryDummyValue   = rand.Float64()*1e9
-
+		cpuDummyValue      = 0.1 // use the time since it's a monotonically increasing value
+		memoryDummyValue   = 0.1
 	)
 
-	// print the cpu and memory dummy values
-	log.G(context.Background()).Infof("cpuDummyValue: %v", cpuDummyValue)
-	log.G(context.Background()).Infof("memoryDummyValue: %v", memoryDummyValue)
+	userTime, systemTime, totalCPUTime, err := getUserSystemTotalCPUTime()
+	if err != nil {
+		fmt.Println("Error getting user, system, and total CPU time:", err)
+	} else {
+		cpuDummyValue = userTime + systemTime
+		fmt.Printf("CPU Usage (User): %.2f\n", userTime)
+		fmt.Printf("CPU Usage (System): %.2f\n", systemTime)
+		fmt.Printf("CPU Usage (Total): %.2f\n", totalCPUTime)
+	}
+		
+	usedMemory, err := getUsedMemory()
+	if err != nil {
+		fmt.Println("Error getting used memory:", err)
+	} else {
+		memoryDummyValue = float64(usedMemory)
+	}
 
 	if metricsMap == nil {
 		metricsMap = map[string][]*dto.Metric{}
@@ -644,9 +666,10 @@ func (p *MockProvider) GetMetricsResource(ctx context.Context) ([]*dto.MetricFam
 	defer span.End()
 
 	var (
-		nodeNameStr      = "NodeName"
-		podNameStr       = "PodName"
-		containerNameStr = "containerName"
+		nodeNameStr      = "node"
+		podNameStr       = "pod"
+		containerNameStr = "container"
+		namespaceStr     = "namespace"
 	)
 	nodeLabels := []*dto.LabelPair{
 		{
@@ -666,6 +689,10 @@ func (p *MockProvider) GetMetricsResource(ctx context.Context) ([]*dto.MetricFam
 				Name:  &podNameStr,
 				Value: &pod.Name,
 			},
+			{
+				Name:  &namespaceStr,
+				Value: &pod.Namespace,
+			},
 		}
 		metricsMap = p.generateMockMetrics(metricsMap, "pod", podLabels)
 		for _, container := range pod.Spec.Containers {
@@ -673,6 +700,10 @@ func (p *MockProvider) GetMetricsResource(ctx context.Context) ([]*dto.MetricFam
 				{
 					Name:  &nodeNameStr,
 					Value: &p.nodeName,
+				},
+				{
+					Name:  &namespaceStr,
+					Value: &pod.Namespace,
 				},
 				{
 					Name:  &podNameStr,
@@ -738,4 +769,31 @@ func addAttributes(ctx context.Context, span trace.Span, attrs ...string) contex
 		ctx = span.WithField(ctx, attrs[i], attrs[i+1])
 	}
 	return ctx
+}
+
+
+func getUserSystemTotalCPUTime() (float64, float64, float64, error) {
+	cpuTimes, err := cpu.Times(false)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	var userTime, systemTime, totalCPUTime float64
+	for _, ct := range cpuTimes {
+		userTime += ct.User
+		systemTime += ct.System
+		totalCPUTime += ct.Total()
+		fmt.Printf("CPU Time (User): %.2f\n", ct.User)
+	}
+
+	return userTime, systemTime, totalCPUTime, nil
+}
+
+
+func getUsedMemory() (uint64, error) {
+	memInfo, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, err
+	}
+	return memInfo.Used, nil
 }
