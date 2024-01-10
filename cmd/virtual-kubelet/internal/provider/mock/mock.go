feab81b4b688cc19363e15014c18f6b230a06f8b
tsaie79 <tsaie79@gmail.com>
2024-01-10 15:57:37 +0000

    Remove unused imports and functions related to generating mock metrics

--- cmd/virtual-kubelet/internal/provider/mock/mock.go
+++ cmd/virtual-kubelet/internal/provider/mock/mock.go
@@ -27,9 +27,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	// "github.com/pkg/errors"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
-
 )
 
 const (
@@ -584,65 +581,6 @@ func (p *MockProvider) GetStatsSummary(ctx context.Context) (*stats.Summary, err
 }
 
 
-func (p *MockProvider) generateMockMetrics(metricsMap map[string][]*dto.Metric, resourceType string, label []*dto.LabelPair) map[string][]*dto.Metric {
-	var (
-		cpuMetricSuffix    = "_cpu_usage_seconds_total" // the rate of change of this metric is the cpu usage
-		memoryMetricSuffix = "_memory_working_set_bytes"
-		cpuDummyValue      = 0.1 // use the time since it's a monotonically increasing value
-		memoryDummyValue   = 0.1
-	)
-
-	userTime, systemTime, totalCPUTime, err := getUserSystemTotalCPUTime()
-	if err != nil {
-		fmt.Println("Error getting user, system, and total CPU time:", err)
-	} else {
-		cpuDummyValue = userTime + systemTime
-		fmt.Printf("CPU Usage (User): %.2f\n", userTime)
-		fmt.Printf("CPU Usage (System): %.2f\n", systemTime)
-		fmt.Printf("CPU Usage (Total): %.2f\n", totalCPUTime)
-	}
-		
-	usedMemory, err := getUsedMemory()
-	if err != nil {
-		fmt.Println("Error getting used memory:", err)
-	} else {
-		memoryDummyValue = float64(usedMemory)
-	}
-
-	if metricsMap == nil {
-		metricsMap = map[string][]*dto.Metric{}
-	}
-
-	finalCpuMetricName := resourceType + cpuMetricSuffix
-	finalMemoryMetricName := resourceType + memoryMetricSuffix
-
-	newCPUMetric := dto.Metric{
-		Label: label,
-		Counter: &dto.Counter{
-			Value: &cpuDummyValue,
-		},
-	}
-	newMemoryMetric := dto.Metric{
-		Label: label,
-		Gauge: &dto.Gauge{
-			Value: &memoryDummyValue,
-		},
-	}
-	// if metric family exists add to metric array
-	if cpuMetrics, ok := metricsMap[finalCpuMetricName]; ok {
-		metricsMap[finalCpuMetricName] = append(cpuMetrics, &newCPUMetric)
-	} else {
-		metricsMap[finalCpuMetricName] = []*dto.Metric{&newCPUMetric}
-	}
-	if memoryMetrics, ok := metricsMap[finalMemoryMetricName]; ok {
-		metricsMap[finalMemoryMetricName] = append(memoryMetrics, &newMemoryMetric)
-	} else {
-		metricsMap[finalMemoryMetricName] = []*dto.Metric{&newMemoryMetric}
-	}
-
-	return metricsMap
-}
-
 func (p *MockProvider) getMetricType(metricName string) *dto.MetricType {
 	var (
 		dtoCounterMetricType = dto.MetricType_COUNTER
@@ -769,31 +707,4 @@ func addAttributes(ctx context.Context, span trace.Span, attrs ...string) contex
 		ctx = span.WithField(ctx, attrs[i], attrs[i+1])
 	}
 	return ctx
-}
-
-
-func getUserSystemTotalCPUTime() (float64, float64, float64, error) {
-	cpuTimes, err := cpu.Times(false)
-	if err != nil {
-		return 0, 0, 0, err
-	}
-
-	var userTime, systemTime, totalCPUTime float64
-	for _, ct := range cpuTimes {
-		userTime += ct.User
-		systemTime += ct.System
-		totalCPUTime += ct.Total()
-		fmt.Printf("CPU Time (User): %.2f\n", ct.User)
-	}
-
-	return userTime, systemTime, totalCPUTime, nil
-}
-
-
-func getUsedMemory() (uint64, error) {
-	memInfo, err := mem.VirtualMemory()
-	if err != nil {
-		return 0, err
-	}
-	return memInfo.Used, nil
 }
