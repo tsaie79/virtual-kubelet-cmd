28af1ddf75036258affafcab32ced14f6aed9e4e
tsaie79 <tsaie79@gmail.com>
2024-02-01 18:51:20 +0000

    Remove unused code in start.sh

--- test-run/start.sh
+++ test-run/start.sh
@@ -13,8 +13,5 @@ export JIRIAF_WALLTIME="60" # set multiple of 60
 export JIRIAF_NODETYPE="cpu"
 export JIRIAF_SITE="Local"
 
-# echo "{\"$NODENAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > $HOME/.host-cfg.json
 
 "$MAIN/virtual-kubelet-cmd" --nodename $NODENAME --provider mock --klog.v 3 > ./$NODENAME.log 2>&1 
-# "$VK_BIN/virtual-kubelet" --nodename $NODENAME --provider mock --provider-config $HOME/.host-cfg.json --log-level debug --klog.v 3 > ./vk.log 2>&1 
-# 
