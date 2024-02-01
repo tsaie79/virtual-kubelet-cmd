5cb1aa1266caced3253710eeec52ed924f0d8435
tsaie79 <tsaie79@gmail.com>
2024-02-01 18:57:11 +0000

    Update start.sh script to use VK_BIN environment variable
    This commit updates the start.sh script to use the VK_BIN environment variable instead of hardcoding the path to the virtual-kubelet binary. This allows for more flexibility and easier maintenance of the script.

--- test-run/start.sh
+++ test-run/start.sh
@@ -2,6 +2,7 @@
 
 export MAIN="/workspaces/virtual-kubelet-cmd"
 export VK_PATH="$MAIN/test-run/apiserver"
+export VK_BIN="$MAIN/bin"
 export KUBECONFIG="$HOME/.kube/config"
 export VKUBELET_POD_IP="172.17.0.1" # "10.250.64.71"
 export APISERVER_CERT_LOCATION="$VK_PATH/client.crt"
@@ -14,4 +15,4 @@ export JIRIAF_NODETYPE="cpu"
 export JIRIAF_SITE="Local"
 
 
-"$MAIN/virtual-kubelet-cmd" --nodename $NODENAME --provider mock --klog.v 3 > ./$NODENAME.log 2>&1 
+"$VK_BIN/virtual-kubelet" --nodename $NODENAME --provider mock --klog.v 3 > ./$NODENAME.log 2>&1 
